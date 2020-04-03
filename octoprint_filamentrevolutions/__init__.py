6cd1c56b2422ce5e1385afd4702390a7598991ad
katihatefi <63093550+katihatefi@users.noreply.github.com>
2020-04-03 16:58:29 +0430

    Update __init__.py

--- octoprint_filamentrevolutions/__init__.py
+++ octoprint_filamentrevolutions/__init__.py
@@ -8,7 +8,7 @@ from time import sleep
 from flask import jsonify
 
 
-class ComputerVision3dprinter(octoprint.plugin.StartupPlugin,
+class FilamentSensorsRevolutions(octoprint.plugin.StartupPlugin,
                                  octoprint.plugin.EventHandlerPlugin,
                                  octoprint.plugin.TemplatePlugin,
                                  octoprint.plugin.SettingsPlugin,
@@ -129,7 +129,7 @@ class ComputerVision3dprinter(octoprint.plugin.StartupPlugin,
             overfilled_pause_print=True,
 
             mode=0,    # Board Mode
-            send_gcode_only_once=False,  # Default set to False for backward compatibility
+            send_gcode_only_once=True,  # Default set to False for backward compatibility
         )
 
     def on_settings_save(self, data):
